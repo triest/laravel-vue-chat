2c4c77ab4af3c070f109ceb5a27b104d8e931692
triest <you@example.com>
2019-02-10 18:08:15 +0300

    First commit

---
+++ resources/assets/js/components/ChatMessage.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+    <div class="chat-message">
+        <p>{{ message.message }}</p>
+        <small><b>{{ message.user.name }}</b></small>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['message']
+    }
+</script>
+
+<style lang="css">
+    .chat-message {
+        padding: 1rem;
+    }
+
+    .chat-message > p {
+        margin-bottom: .5rem;
+    }
+</style>
